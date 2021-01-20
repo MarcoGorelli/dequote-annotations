13797d2763c84bd36a95389404eafda58a825e9d
Marco Gorelli <marcogorelli@protonmail.com>
2021-01-20 10:04:57 +0000

    add test files / configs

---
+++ tests/data/t_py38_expected.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def func(z, /, a: None, *, b: str) -> List[str, str]:
+    def funcfunc(a: str):
+        def funcfuncfunc(a: str):
+            pass
+
+class Cat:
+
+    a: str
+    b: str
+
+    def func2(b: str, c: str):
+        def func2func(d: int):
+            pass
+
+    class Bat:
+        e: int
---
+++ tests/main_test.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+import sys
+
+import pytest
+
+from no_string_hints import main
+
+
+def test_main(tmpdir):
+    test_file = os.path.join('tests', 'data', 't.py')
+    tmp_test_file = os.path.join(tmpdir, 't.py')
+    shutil.copy(test_file, tmp_test_file)
+
+    main((tmp_test_file,))
+
+    with open(tmp_test_file) as fd:
+        result = fd.read()
+
+    with open(os.path.join('tests', 'data', 't_expected.py')) as fd:
+        expected = fd.read()
+
+    assert result == expected
+
+
+@pytest.mark.skipif(
+    sys.version_info.major != 3 or sys.version_info.minor != 8,
+    reason='posonly args not available in Python3.7',
+)
+def test_posonly_kwonly(tmpdir):
+    test_file = os.path.join('tests', 'data', 't_py38.py')
+    tmp_test_file = os.path.join(tmpdir, 't.py')
+    shutil.copy(test_file, tmp_test_file)
+
+    main((tmp_test_file,))
+
+    with open(tmp_test_file) as fd:
+        result = fd.read()
+
+    with open(os.path.join('tests', 'data', 't_py38_expected.py')) as fd:
+        expected = fd.read()
+
+    assert result == expected
